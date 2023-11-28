bd5368291bea44c158824350759ff5be660084c1
vince <vincemarc.mr@gmail.com>
2023-11-29 03:21:08 +0800

    bot mentions fixed

--- src/index.js
+++ src/index.js
@@ -20,17 +20,11 @@ client.on('interactionCreate', async (interaction) => {
 
     if (interaction.commandName === 'announce') {
         const sendto = interaction.options.get('sendto').value;
-        const mention = interaction.options.get('mention').value;
+        const mention = interaction.options.getMentionable('mention');
         const message = interaction.options.get('message').value;
-        // const mentionString = mention === 'everyone' ? '@everyone' : `<@${mention}>`;
-
-
-        const channelId = sendto.replace(/<#|>/g, '');
-        const channel = interaction.guild.channels.cache.get(channelId);
 
         if (channel) {
-            // await channel.send(`<@${mention}>`);
-            await channel.send(`<@${mention}> \n\n ${message}`);
+            await channel.send(`${mention} \n\n ${message}`);
             interaction.reply(`Message sent to <#${channelId}>: ${message}`);
         } else {
             interaction.reply(`Invalid channel mention: ${sendto}`);
